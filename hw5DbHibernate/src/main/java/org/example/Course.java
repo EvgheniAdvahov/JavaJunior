24466e5ef5ee5781b1523d491d224911b6974ecf
EvgheniAdvahov <evghenii.88@gmail.com>
2024-04-06 13:02:49 +0300

    add to db

--- hw5DbHibernate/src/main/java/org/example/Course.java
+++ hw5DbHibernate/src/main/java/org/example/Course.java
@@ -1,15 +1,34 @@
 package org.example;
 
 import javax.persistence.*;
+import java.util.Random;
 
 @Entity
 @Table(name = "Courses")
 public class Course {
 
+    private static final Random random = new Random();
+    private static final String[] courses = new String[]{"Java Junior", "Python", "JavaScript", "Java Spring", "SQL"};
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    private String duration;
+    private int duration;
+
+    public Course(Long id, String title, int duration) {
+        this.id = id;
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public Course(String title, int duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public static Course create(){
+        return new Course(courses[random.nextInt(courses.length)], random.nextInt(30, 60));
+    }
 
 }
